cfe24fa8042ab9188b0b339e008bf2d06d3d771f
Mathieu Comandon <strycore@gmail.com>
2015-11-25 17:11:49 -0800

    Make window width and height class attributes

--- common.py
+++ common.py
@@ -2,10 +2,13 @@ from gi.repository import Gtk
 
 
 class Window(Gtk.Window):
+    width = 400
+    height = 400
+
     def __init__(self):
         super(Window, self).__init__()
         self.connect('destroy', Gtk.main_quit)
-        self.set_default_size(400, 400)
+        self.set_default_size(self.width, self.height)
         self.set_position(Gtk.WindowPosition.CENTER)
         self.post_init()
         self.show_all()
